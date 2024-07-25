a0f79f22d17303e90a05489cdf15f912094ef360
Gunseli Unsal <gunseliunsal@gmail.com>
2024-07-25 12:34:23 +0300

    Improve accessibility

---
+++ WordWizard/AnimatingNumberView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatingNumberView.swift
+//  WordWizard
+//
+//  Created by Günseli Ünsal on 25.07.2024.
+//
+
+import SwiftUI
+
+struct AnimatingNumberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatingNumberView()
+}
